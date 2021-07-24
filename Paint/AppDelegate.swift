c78ea46be2fb0dae4862aa82732fd14782197f37
Илья Мудрый <mudriy.ilya@gmail.com>
2021-07-24 10:51:00 +0300

    Create new project

---
+++ Paint/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Paint
+//
+//  Created by Илья Мудрый on 24.07.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ Paint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Paint
+//
+//  Created by Илья Мудрый on 24.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
